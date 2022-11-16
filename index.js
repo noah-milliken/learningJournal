66eff3c9fa4e6b27e68dc193160442aece3f632d
noah milliken <noahmilliken@gmail.com>
2022-11-16 13:00:37 -0500

    Added the array of objects containing blogposts. added the javascript HTML elements to render the blog posts to the page.

--- index.js
+++ index.js
@@ -1,5 +1,67 @@
-const button = document.getElementById('button')
+const blogPostData = [
+    {
+        date: "July 23, 2022",
+        title: 'Blog One',
+        content: "I'm excited to start a new learning journey as a Scrimba Bootcamp student! After several months of learning in the Frontend Developer Career Path.",
+        imagePath: "blog-image-06.png",
+        imageAlt: 'Picture of a computer and desk',
+    },
+    {
+        date: "July 23, 2022",
+        title: 'Blog One',
+        content: "I'm excited to start a new learning journey as a Scrimba Bootcamp student! After several months of learning in the Frontend Developer Career Path.",
+        imagePath: "blog-image-05.png",
+        imageAlt: 'Picture of a computer and desk',
+    },
+    {
+        date: "July 23, 2022",
+        title: 'Blog One',
+        content: "I'm excited to start a new learning journey as a Scrimba Bootcamp student! After several months of learning in the Frontend Developer Career Path.",
+        imagePath: "blog-image-04.png",
+        imageAlt: 'Picture of a computer and desk',
+    },
+    {
+        date: "July 23, 2022",
+        title: 'Blog One',
+        content: "I'm excited to start a new learning journey as a Scrimba Bootcamp student! After several months of learning in the Frontend Developer Career Path.",
+        imagePath: "blog-image-03.png",
+        imageAlt: 'Picture of a computer and desk',
+    },
+    {
+        date: "July 23, 2022",
+        title: 'Blog One',
+        content: "I'm excited to start a new learning journey as a Scrimba Bootcamp student! After several months of learning in the Frontend Developer Career Path.",
+        imagePath: "blog-image-02.png",
+        imageAlt: 'Picture of a computer and desk',
+    },
+    {
+        date: "July 23, 2022",
+        title: 'Blog One',
+        content: "I'm excited to start a new learning journey as a Scrimba Bootcamp student! After several months of learning in the Frontend Developer Career Path.",
+        imagePath: "blog-image-01.png",
+        imageAlt: 'Picture of a computer and desk',
+    },
+]
 
-button.addEventListener('click', function () {
-    console.log('I ate all the banannas')
-})
+
+function getBlogHtml() {
+
+    let blogHtml = ``
+    blogPostData.forEach(function (post) {
+        blogHtml += `
+        <div class="post">
+            <img src="${post.imagePath}" alt="${post.imageAlt}">
+            <p>${post.date}</p>
+            <h3>${post.title}</h3>
+            <p>${post.content}</p>
+        </div>
+        `
+    })
+    return blogHtml
+
+}
+function render() {
+    const blogFeed = document.getElementById('blog-feed')
+    blogFeed.innerHTML = getBlogHtml()
+}
+render()
