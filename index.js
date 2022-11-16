a41a1c7fafd15c3d2d4d9d511f87b92377da9fb7
noah milliken <noahmilliken@gmail.com>
2022-11-16 13:44:27 -0500

    Updated the blog post to grid

--- index.js
+++ index.js
@@ -1,41 +1,41 @@
 const blogPostData = [
     {
-        date: "July 23, 2022",
-        title: 'Blog One',
+        date: "July 21, 2022",
+        title: 'Blog Six',
         content: "I'm excited to start a new learning journey as a Scrimba Bootcamp student! After several months of learning in the Frontend Developer Career Path.",
         imagePath: "blog-image-06.png",
         imageAlt: 'Picture of a computer and desk',
     },
     {
-        date: "July 23, 2022",
-        title: 'Blog One',
+        date: "July 14, 2022",
+        title: 'Blog Five',
         content: "I'm excited to start a new learning journey as a Scrimba Bootcamp student! After several months of learning in the Frontend Developer Career Path.",
         imagePath: "blog-image-05.png",
         imageAlt: 'Picture of a computer and desk',
     },
     {
-        date: "July 23, 2022",
-        title: 'Blog One',
+        date: "July 7, 2022",
+        title: 'Blog Four',
         content: "I'm excited to start a new learning journey as a Scrimba Bootcamp student! After several months of learning in the Frontend Developer Career Path.",
         imagePath: "blog-image-04.png",
         imageAlt: 'Picture of a computer and desk',
     },
     {
-        date: "July 23, 2022",
-        title: 'Blog One',
+        date: "June 31, 2022",
+        title: 'Blog Three',
         content: "I'm excited to start a new learning journey as a Scrimba Bootcamp student! After several months of learning in the Frontend Developer Career Path.",
         imagePath: "blog-image-03.png",
         imageAlt: 'Picture of a computer and desk',
     },
     {
-        date: "July 23, 2022",
-        title: 'Blog One',
+        date: "June 24, 2022",
+        title: 'Blog Two',
         content: "I'm excited to start a new learning journey as a Scrimba Bootcamp student! After several months of learning in the Frontend Developer Career Path.",
         imagePath: "blog-image-02.png",
         imageAlt: 'Picture of a computer and desk',
     },
     {
-        date: "July 23, 2022",
+        date: "June 17, 2022",
         title: 'Blog One',
         content: "I'm excited to start a new learning journey as a Scrimba Bootcamp student! After several months of learning in the Frontend Developer Career Path.",
         imagePath: "blog-image-01.png",
@@ -51,9 +51,11 @@ function getBlogHtml() {
         blogHtml += `
         <div class="post">
             <img src="${post.imagePath}" alt="${post.imageAlt}">
-            <p>${post.date}</p>
-            <h3>${post.title}</h3>
-            <p>${post.content}</p>
+            <div class="blog-text">
+                <p>${post.date}</p>
+                <h3>${post.title}</h3>
+                <p>${post.content}</p>
+            </div>
         </div>
         `
     })
